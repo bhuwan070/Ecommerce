b4d1345ac9cc996bc399bfc43496d94a45fb5dc7
bhuwanoj_ <bhuwanojha10@gmail.com>
2025-02-13 23:57:44 +0545

    about

--- src/Components/About/AboutHero.jsx
+++ src/Components/About/AboutHero.jsx
@@ -4,14 +4,36 @@ import AboutHeroimg from "../../assets/Banner/about-1.png";
 const AboutHero = () => {
   return (
     <section>
-      <div className="h-[100vh] flex flex-1">
+      <div className="h-[100vh] w-full grid grid-cols-1 md:grid-cols-2">
         {/* image section */}
-        <div className="pl-32 py-10">
-          <img className="h-[100vh] rounded-xl" src={AboutHeroimg} alt="" />
+        <div className=" h-full w-full pl-32 sm:px-16 py-10">
+          <img
+            className="h-[100vh] w-full rounded-xl"
+            src={AboutHeroimg}
+            alt=""
+          />
         </div>
         {/* text and slider */}
-        <div>
-          <h1>Welcome to Nest</h1>
+        <div className="h-full w-full pt-20">
+          <h1 className=" text-3xl sm:text-4xl md:text-5xl font-bold text-gray-700 py-5 text-start">
+            Welcome to Nest
+          </h1>
+          <p className="py-5 pr-8">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate id est laborum.
+          </p>
+          <p className="pr-8">
+            Ius ferri velit sanctus cu, sed at soleat accusata. Dictas prompta
+            et Ut placerat legendos interpre.Donec vitae sapien ut libero
+            venenatis faucibus. Nullam quis ante Etiam sit amet orci eget. Quis
+            commodo odio aenean sed adipiscing. Turpis massa tincidunt dui ut
+            ornare lectus. Auctor elit sed vulputate mi sit amet. Commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate id
+            est laborum.
+          </p>
         </div>
       </div>
     </section>
