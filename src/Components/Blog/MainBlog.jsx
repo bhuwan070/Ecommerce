b113220afa47df4c600b275d491d373b12d5b21b
Biswash <koiralabiswash123@gmail.com>
2025-03-01 01:23:21 +0545

    updated blog section

--- src/Components/Blog/MainBlog.jsx
+++ src/Components/Blog/MainBlog.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import Category1 from "../../assets/Blog/category-1.svg";
+import { FaSearch } from "react-icons/fa";
 
 const MainBlog = () => {
   return (
-    <>
-    
-    </>
-  )
-}
+    <section className="my-10 grid grid-cols-4">
+      <div className="px-4 col-span-3">
+        <div className="flex px-2 gap-4">
+          <img className="w-10" src={Category1} alt="" />
+          <h1 className="text-[40px] text-gray-700 font-bold tracking-wide">
+            Recips Articles
+          </h1>
+        </div>
+      </div>
+      <div className="col-span-1 px-4">
+        <div className="w-full p-4 flex justify-between items-center border-2 rounded-lg shadow-md">
+          <input
+            type="text"
+            placeholder="Search for items..."
+            className="border-none focus:outline-none w-[75%]"
+          />
+          <FaSearch className="hover:rotate-90 ease-out duration-500" size={20}/>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default MainBlog
+export default MainBlog;
